d89b1cd03b38a8acc9cd4f8da778f3e7d2940b3a
GruffCassquatch <cassanda.agius@gmail.com>
2021-10-10 12:06:47 +1100

    Update for TLD v1.95

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using MelonLoader;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ClothingTweaker")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,7 +33,7 @@ using MelonLoader;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2")]
-[assembly: AssemblyFileVersion("2.2")]
-[assembly: MelonInfo(typeof(ClothingTweaker.Implementation), "ClothingTweaker", "2.2", "GruffCassquatch")]
+[assembly: AssemblyVersion("2.3")]
+[assembly: AssemblyFileVersion("2.3")]
+[assembly: MelonInfo(typeof(ClothingTweaker.Implementation), "ClothingTweaker", "2.3", "GruffCassquatch")]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
